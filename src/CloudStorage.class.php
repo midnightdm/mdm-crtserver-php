d4ca893441f4685e256338941236f62873477c1a
Brian Griffin <bgtalkingdog@gmail.com>
2021-10-16 21:01:08 -0500

    var name fixes

--- src/CloudStorage.class.php
+++ src/CloudStorage.class.php
@@ -39,13 +39,11 @@ class CloudStorage {
     }
 
     public function upload($sourcePath, $destName) {    
-        $this->sourcePath = $sourcePath;       
-        $this->destName   = $destName;
         if(is_readable($sourcePath)) {
             $file = fopen($sourcePath, 'r');
             $bucket = $this->storage->bucket($this->bucketName);
             $object = $bucket->upload($file, ['name' => $destName]);
-            flog("Uploaded ".basename($sourcePath)." to gs://".$this->bucketName."/".$desttName."\n");
+            flog("Uploaded ".basename($sourcePath)." to gs://".$this->bucketName."/".$destName."\n");
 
         }
         

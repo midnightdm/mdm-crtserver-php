34f98622fdf804ff7524ab1db03313eeb2556995
Brian Griffin <bgtalkingdog@gmail.com>
2021-11-21 18:01:52 -0600

    Added tracers to test Alerts objectQueue

--- src/AlertsModel.class.php
+++ src/AlertsModel.class.php
@@ -247,7 +247,11 @@ class AlertsModel extends Firestore {
         $this->db->collection('Alertpublish')->document(strval($apubID))->set($data);
         //Also update collective alert list queue (a or p type)... 
         $ref = $type=='p' ? 'alertsPassenger' : 'alertsAll';
+        $len = count($this->daemon->$ref);
+        flog("Last $ref obj in $len sized array before queue update:". $this->daemon->$ref[$len]."\n");
         $this->daemon->$ref = objectQueue($this->daemon->$ref, $data);
+        $len = count($this->daemon->$ref);
+        flog("Last $ref obj in $len sized array after queue update:". $this->daemon->$ref[$len]."\n");
         //...and save as db document
         $sref = $type=='p' ? 'setAlertsPassenger' : 'setAlertsAll';
         $this->$sref($this->daemon->$ref);

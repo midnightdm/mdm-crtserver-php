805f15b60a2288fd5ee7498373da0aba4748a897
Brian Griffin <bgtalkingdog@gmail.com>
2022-02-02 21:17:51 -0600

    removed tracer

--- src/LiveScanModel.class.php
+++ src/LiveScanModel.class.php
@@ -24,7 +24,7 @@ class LiveScanModel extends Firestore {
 
 
     public function insertLiveScan($live) {
-        flog("insertLiveScan(live) DATA=". var_dump($live). "EOF"); //Test Only
+        //flog("insertLiveScan(live) DATA=". $live. "EOF"); //Test Only
         $this->db->collection('LiveScan')->document('mmsi'.$live['liveVesselID'])->set($live);
     }
 

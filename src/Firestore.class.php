a4cb3a172af211b23369d92cc6b53530050c1813
Brian Griffin <admin@clintonrivertraffic.com>
2023-05-25 21:37:25 -0500

    added tracer to class init

--- src/Firestore.class.php
+++ src/Firestore.class.php
@@ -17,7 +17,9 @@ use Google\Cloud\Firestore\FieldValue;
 
 //echo $_SERVER['DOCUMENT_ROOT'] . $config['firestore_json_file'];
 if(!isset($strJsonFileContents)) {
-  $strJsonFileContents = file_get_contents($_SERVER['DOCUMENT_ROOT'] . $config['firestore_json_file']);
+  $path =  $_SERVER['DOCUMENT_ROOT'] . $config['firestore_json_file'];
+  flog("INIT: Firestore loading json from $path \n"); 
+  $strJsonFileContents = file_get_contents($path);
   //Convert into array & Put into CONSTANT
   define('GOOGLE_APPLICATION_CREDENTIALS', json_decode($strJsonFileContents, true));
 }
